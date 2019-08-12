2eba77a2c3c15982f5ed8d1d32c3b6bdb3b30e93
murphy <murphy@iffy.me>
2019-08-12 21:12:21 +0800

    fzf histdb

--- .fzf/shell/key-bindings.zsh
+++ .fzf/shell/key-bindings.zsh
@@ -83,3 +83,21 @@ zle     -N   fzf-history-widget
 #bindkey '^R' fzf-history-widget
 
 fi
+
+histdb-fzf-widget() {
+  local selected num
+  setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
+  selected=( $(histdb --sep ,,, | awk -F',,,' '{print $1,$3,$4}' |
+    FZF_DEFAULT_OPTS="--header-lines=1 --height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd) |
+    cut --delimiter=' ' --field=3-)
+    )
+
+  LBUFFER=$selected
+  zle redisplay
+  typeset -f zle-line-init >/dev/null && zle zle-line-init
+
+  return $ret
+}
+
+zle     -N   histdb-fzf-widget
+bindkey '^R' histdb-fzf-widget
